b238709fefb4eb34b1e62ad44fd1d2860e67a902
LiXianglong0 <86600433+LiXianglong0@users.noreply.github.com>
2021-06-28 18:17:38 +0800

    粒子群算法C#实现

---
+++ PSOTest2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSOTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPSO pso = new MyPSO();
+            pso.run();
+            //GeneticAlgorithm gen = new GeneticAlgorithm();
+            //gen.Run();
+        }
+    }
+}
+
